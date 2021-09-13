8925b5a7f437b38c4252205d6508541aafb81a7c
Sarvesh Ransubhe <sarvesh.ransubhe21@gmail.com>
2021-09-13 11:06:12 +0530

    Day 21 Completed

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -26,9 +26,6 @@ class CartModel {
 
   // Remove Item
 
-  void remove(Item item) {
-    _itemIds.remove(item.id);
-  }
 }
 
 class AddMutation extends VxMutation<MyStore> {
@@ -38,5 +35,15 @@ class AddMutation extends VxMutation<MyStore> {
   @override
   perform() {
     store.cart._itemIds.add(item.id);
+  }
+}
+
+class RemoveMutation extends VxMutation<MyStore> {
+  final Item item;
+
+  RemoveMutation(this.item);
+  @override
+  perform() {
+    store.cart._itemIds.remove(item.id);
   }
 }
